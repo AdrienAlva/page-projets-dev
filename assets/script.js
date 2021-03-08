02842931d16b28d05ed79fcaac92d1d779c49cd0
Alvarez <jamespatageules@hotmail.com>
2021-03-08 09:11:42 +0100

    Galleries split

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,60 @@
 
 // Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
+function openModal(gallery_id) {
+  if(gallery_id == 1) {
+    document.getElementById("myModal_1").style.display = "block";    
+  } else if (gallery_id == 2){
+    document.getElementById("myModal_2").style.display = "block";
+  } else {
+    return;
+  }
+}//Eo openModal()
 
 // Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
+function closeModal(gallery_id) {
+  if(gallery_id == 1) {
+    document.getElementById("myModal_1").style.display = "none";    
+  } else if (gallery_id == 2){
+    document.getElementById("myModal_2").style.display = "none";
+  } else {
+    return;
+  }
+}//Eo closeModal()
 
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex, 0);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, gallery_id) {
+  showSlides(slideIndex += n, gallery_id);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, gallery_id) {
+  showSlides(slideIndex = n, gallery_id);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+function showSlides(n, gallery_id) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides_" + gallery_id.toString());
+
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+
+  if (gallery_id > 0) {
+      slides[slideIndex-1].style.display = "block";
+  }
+  
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
+}//Eo showSlides()
